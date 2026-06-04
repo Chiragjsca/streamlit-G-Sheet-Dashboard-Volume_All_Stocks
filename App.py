b2df45acb166dbfc8ffa5db7549c2a932a64ac2b
Chiragjsca <chiragjsca@gmail.com>
2026-06-05 00:48:04 +0530

    Update App.py

--- App.py
+++ App.py
@@ -443,7 +443,7 @@ search_query = st.sidebar.text_input("Search by Symbol, Name, etc...", key="sear
 
 st.sidebar.markdown("---")
 st.sidebar.header("📑 Select a Tab")
-sheet_names = ["Top 250 Stocks", "Final List", "Final List 2", "-Diff @ 200 DMA", "+Diff @ 200 DMA", "52W Low-GTT", "+%", "-%","NSE Fundamentals"]
+sheet_names = ["NSE Price Data","NSE Fundamentals", "Final List", "Final List 2", "-Diff @ 200 DMA", "+Diff @ 200 DMA", "52W Low-GTT", "+%", "-%"]
 selected_sheet = st.sidebar.selectbox("Choose sheet", sheet_names, key="filter_sheet")
 
 # ---------- Main Execution ----------
